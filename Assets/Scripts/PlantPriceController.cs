f55da8d84b6db22b0216dee920b2e7260f65dba2
Olexandra_Panibratets <kn221_pod@student.ztu.edu.ua>
2025-05-31 20:38:37 +0300

    відображення ціни при натисканні на рослинку в AR-сцені або на image  з описом

---
+++ Assets/Scripts/PlantPriceController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.InputSystem;
+
+public class PlantPriceController : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private GameObject priceLabel;
+    [SerializeField] private string priceText = "Ціна: 100 грн";
+
+    private bool _isPriceVisible = false;
+
+    void Start()
+    {
+        if (priceLabel != null)
+            priceLabel.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Touchscreen.current == null || Touchscreen.current.primaryTouch.press.wasPressedThisFrame == false)
+            return;
+
+        if (EventSystem.current.IsPointerOverGameObject())
+            return;
+
+        Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+        Ray ray = Camera.main.ScreenPointToRay(touchPosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform == transform)
+            {
+                TogglePrice();
+            }
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        TogglePrice();
+    }
+
+    public void TogglePrice()
+    {
+        _isPriceVisible = !_isPriceVisible;
+
+        if (priceLabel != null)
+        {
+            priceLabel.SetActive(_isPriceVisible);
+            if (_isPriceVisible)
+                priceLabel.GetComponent<Text>().text = priceText;
+        }
+    }
+}
